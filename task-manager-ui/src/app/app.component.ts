3d8379e3ff3989efca2c5372edb2b783cae922ba
Bennet Otoho <bennet2000@outlook.com>
2025-01-18 00:19:24 +0100

    reconfigure Angular app

--- task-manager-ui/src/app/app.component.ts
+++ task-manager-ui/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TaskComponent],
   template: `
-    <h1>Default</h1>
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-task></app-task>
+      </section>
+    </main>
   `,
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'tasks';
 }
