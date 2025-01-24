f63010c37ddb673d6c0e12afcf3ffa7f034bf8dc
Bennet Otoho <bennet2000@outlook.com>
2025-01-24 11:56:43 +0100

    setup home toolbar

--- task-manager-ui/src/app/app.component.ts
+++ task-manager-ui/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core'
-
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { TaskComponent } from './task/task.component'
 
 @Component({
   selector: 'app-root',
-  imports: [TaskComponent],
+  imports: [TaskComponent, MatToolbarModule, MatButtonModule, MatIconModule],
   template: `
     <main>
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        <mat-toolbar class="justify-content-between">
+          <div>
+            <a href="/"><img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" /></a>
+          </div>
+          <div>
+            <button mat-icon-button class="profile-button" aria-label="Profile icon">
+              <mat-icon>account_circle</mat-icon>
+            </button>
+          </div>
+        </mat-toolbar>
       </header>
       <section class="content">
         <app-task></app-task>
